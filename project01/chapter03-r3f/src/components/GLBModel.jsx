d9ee30ca6682853760a6e100a70f59277fc9e016
3uxeca <sajihye4@gmail.com>
2025-03-09 18:44:30 +0900

    - R3F로 애니메이션 구현하기 실습

--- project01/chapter03-r3f/src/components/GLBModel.jsx
+++ project01/chapter03-r3f/src/components/GLBModel.jsx
@@ -1,8 +1,14 @@
-import { useGLTF } from "@react-three/drei";
-import { useEffect } from "react";
+import { useAnimations, useGLTF } from "@react-three/drei";
+import { useFrame, useThree } from "@react-three/fiber";
+import { useEffect, useRef } from "react";
 
 export const GLBModel = () => {
-  const { scene } = useGLTF("/dancer.glb");
+  const three = useThree();
+  console.log("three :: ", three);
+  const { scene, animations } = useGLTF("/dancer.glb");
+  const ref = useRef(null);
+
+  const { actions } = useAnimations(animations, ref);
 
   useEffect(() => {
     scene.traverse((obj) => {
@@ -11,8 +17,16 @@ export const GLBModel = () => {
         obj.receiveShadow = true;
       }
     });
-  }, [scene]);
+    console.log("actions", actions);
+    actions["wave"].play();
+  }, [scene, actions]);
+
+  useFrame((state, delta) => {
+    // console.log(state);
+    // console.log(delta); // 현재 프레임과 이전 프레임 간의 간격
+    // ref.current.rotation.y += 0.02;
+  });
 
   // premitive => obj의 geometry 쉽게 해당 형태에 해당하는 mesh 무엇이든 만들어주는 컴포넌트
-  return <primitive scale={0.01} object={scene} position-y={0.8} />;
+  return <primitive ref={ref} scale={0.01} object={scene} position-y={0.8} />;
 };
