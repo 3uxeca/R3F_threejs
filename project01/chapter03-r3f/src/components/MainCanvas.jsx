a5134b7e1eae7343d51363ea257af052d81af0bf
3uxeca <sajihye4@gmail.com>
2025-03-09 20:16:02 +0900

    - 알아두면 유용한 R3F 생태계의 라이브러리 cannon 알아보기

--- project01/chapter03-r3f/src/components/MainCanvas.jsx
+++ project01/chapter03-r3f/src/components/MainCanvas.jsx
@@ -6,6 +6,7 @@ import { Controls } from "./Controls";
 import { GLBModel } from "./GLBModel";
 import { Dancer } from "./Dancer";
 import { PostProcessor } from "./PostProcessor";
+import { Physics } from "@react-three/cannon";
 export const MainCanvas = () => {
   return (
     <Canvas
@@ -27,12 +28,20 @@ export const MainCanvas = () => {
         background: new Color(0x000000),
       }}
     >
+      <Physics
+        gravity={[0, -9, 0]} // 지구처럼 위에서 아래로 중력 작용
+        defaultContactMaterial={{
+          restitution: 0.1, // 탄성력
+          friction: 1, // 마찰력
+        }}
+      >
+        <Lights />
+        <Meshes />
+      </Physics>
       <Controls />
-      <Lights />
-      <Meshes />
       {/* <GLBModel /> */}
-      <PostProcessor />
-      <Dancer />
+      {/* <PostProcessor /> */}
+      {/* <Dancer /> */}
     </Canvas>
   );
 };
--- project01/chapter03-r3f/src/components/Meshes.jsx
+++ project01/chapter03-r3f/src/components/Meshes.jsx
@@ -1,3 +1,4 @@
+import { useBox, useSphere } from "@react-three/cannon";
 import {
   Box,
   Circle,
@@ -8,9 +9,69 @@ import {
   Torus,
   TorusKnot,
 } from "@react-three/drei";
+import { useEffect } from "react";
 import * as THREE from "three";
 
 export const Meshes = () => {
+  const [planeRef] = useBox(() => ({
+    args: [50, 1, 50],
+    type: "Static",
+    mass: 1,
+    position: [0, 0, 0],
+    material: {
+      restitution: 1, // 탄성력
+      friction: 0.5, // 마찰력
+    },
+    onCollide: () => {
+      // 충돌 시 처리
+      console.log("바닥에 충돌했다!");
+    },
+  }));
+
+  // api = 인위적으로 mesh에 영향을 주는 메소드 applyForce : 지속적인 힘, applyImpulse : 한번만 힘
+  const [boxRef, api] = useBox(() => ({
+    args: [1, 1, 1],
+    mass: 1,
+    position: [-1, 2, 0],
+    material: {
+      restitution: 0.4,
+      friction: 0.2,
+    },
+  }));
+
+  const [sphereRef1, sphereApi] = useSphere(() => ({
+    mass: 5,
+    position: [0.5, 8, 0],
+    material: {
+      restitution: 0.4,
+      friction: 0.1,
+    },
+  }));
+
+  const [sphereRef2] = useSphere(() => ({
+    mass: 0.2,
+    position: [1, 5, 0],
+    material: {
+      restitution: 0.2,
+      friction: 0.1,
+    },
+  }));
+
+  useEffect(() => {
+    api.applyForce([555, 50, 0], [1, 0, 0]); // 힘이 진행하는 방향, 전체 좌표에서 진행방향으로 영향을 줌
+    sphereApi.applyLocalForce([-2000, 0, 0], [1, 0, 0]); // 힘을 가하는 방향, 좌표 위치가 mesh 위치 기준으로 영향을 줌
+  }, [api, sphereApi]);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      api.applyLocalImpulse([0, 20, 0], [1, 0, 0]);
+      sphereApi.applyImpulse([200, 10, 0], [0, 0, 0]);
+    }, 3000);
+
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, [api, sphereApi]);
   return (
     <>
       {/* <mesh>
@@ -18,9 +79,9 @@ export const Meshes = () => {
         <meshBasicMaterial color={0xff0000} />
       </mesh> */}
       {/* drei가 기본 제공하는 Mesh 사용 시 자동으로 bufferGeometry로 구현되기 때문에 성능면에서도 더 좋음! */}
-      <Plane args={[40, 40]} rotation-x={-Math.PI / 2} receiveShadow>
+      {/* <Plane args={[40, 40]} rotation-x={-Math.PI / 2} receiveShadow>
         <meshStandardMaterial />
-      </Plane>
+      </Plane> */}
       {/* <Box args={[1, 1, 1]} castShadow position-y={0.5}>
         <meshStandardMaterial color={0xff0000} />
       </Box>
@@ -43,7 +104,7 @@ export const Meshes = () => {
         material-color={"hotpink"}
       /> */}
 
-      <TorusKnot
+      {/* <TorusKnot
         args={[1, 0.2, 128, 128, 2, 3]}
         position={[3, 1.6, 0]}
         castShadow
@@ -56,7 +117,7 @@ export const Meshes = () => {
           emissive={0xffff00}
           emissiveIntensity={2}
         />
-      </TorusKnot>
+      </TorusKnot> */}
       {/* <TorusKnot
         args={[1, 0.2, 128, 128, 2, 3]}
         position={[-7, 1.6, 0]}
@@ -91,6 +152,37 @@ export const Meshes = () => {
       >
         <meshDepthMaterial opacity={0.5} />
       </TorusKnot> */}
+
+      {/* Cannon */}
+      {/* Plane 대신 Box를 사용한 이유 : 물리엔진 이용 시 두께가 없는 mesh를 쓰면 물리엔진 비정상 작동이 발생할 수 있기 때문에! */}
+      <Box ref={planeRef} args={[50, 1, 50]}>
+        <meshStandardMaterial
+          color={0xfefefe}
+          roughness={0.3}
+          metalness={0.8}
+        />
+      </Box>
+      <Box ref={boxRef} args={[1, 1, 1]}>
+        <meshStandardMaterial
+          color={0xff0000}
+          roughness={0.3}
+          metalness={0.8}
+        />
+      </Box>
+      <Sphere ref={sphereRef1}>
+        <meshStandardMaterial
+          color={0x9000ff}
+          roughness={0.3}
+          metalness={0.8}
+        />
+      </Sphere>
+      <Sphere ref={sphereRef2}>
+        <meshStandardMaterial
+          color={0xff00ff}
+          roughness={0.3}
+          metalness={0.8}
+        />
+      </Sphere>
     </>
   );
 };
