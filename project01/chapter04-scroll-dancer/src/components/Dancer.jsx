d37340254c9cb7915f2e9c98a1f2d9b9542c790a
3uxeca <sajihye4@gmail.com>
2025-03-10 11:29:46 +0900

    - gsap 알아보기

--- project01/chapter04-scroll-dancer/src/components/Dancer.jsx
+++ project01/chapter04-scroll-dancer/src/components/Dancer.jsx
@@ -3,8 +3,11 @@ import { IsEnteredAtom } from "../stores"
 import { useEffect, useRef } from "react";
 import { useAnimations, useGLTF, useScroll } from "@react-three/drei";
 import { Loader } from "./Loader";
+import gsap from 'gsap';
+import { useThree } from "@react-three/fiber";
 
 export const Dancer = () => {
+  const three = useThree();
   const isEntered = useRecoilValue(IsEnteredAtom);
   const dancerRef = useRef(null);
   const { scene, animations } = useGLTF('/models/dancer.glb');
@@ -15,6 +18,36 @@ export const Dancer = () => {
     if(!isEntered) return;
     actions['wave'].play();
   }, [actions, isEntered]);
+
+  useEffect(() => {
+    // gsap
+    if(!isEntered) return;
+    if(!dancerRef.current) return;
+    // 카메라 이동동
+    gsap.fromTo(
+      three.camera.position, 
+      {
+        x: -5,
+        y: 5,
+        z: 5,
+      },
+      {
+        duration: 2.5,
+        x: 0,
+        y: 6,
+        z: 12,
+      }
+    )
+    // 카메라 회전
+    gsap.fromTo(
+      three.camera.rotation,
+      { z: Math.PI },
+      {
+        duration: 2.5,
+        z: 0,
+      }
+    )
+  }, [isEntered, three.camera.position, three.camera.rotation]);
 
   if(isEntered) {
     return (
