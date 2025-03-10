869b2fe7ee0c2430dfc9df26582e28d159c741a9
3uxeca <sajihye4@gmail.com>
2025-03-10 10:59:26 +0900

    - 개발환경 재설정정

---
+++ project01/chapter04-scroll-dancer/src/App.jsx
@@ -0,0 +1,20 @@
+import { RecoilRoot } from 'recoil';
+import styled from 'styled-components'
+import { MainCanvas } from './components/MainCanvas';
+function App() {
+  return (
+    <RecoilRoot>
+      <Wrapper id='wrapper'>
+        <MainCanvas />
+      </Wrapper>
+    </RecoilRoot>
+  )
+}
+
+export default App
+
+const Wrapper = styled.div`
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+`;
