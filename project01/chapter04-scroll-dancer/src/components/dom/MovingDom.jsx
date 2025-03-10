e1245222ade660411e0c80f724e755677005d3cb
3uxeca <sajihye4@gmail.com>
2025-03-10 15:22:34 +0900

    - 스크롤 이벤트로 나타나는 DOM 제어하기2

--- project01/chapter04-scroll-dancer/src/components/dom/MovingDom.jsx
+++ project01/chapter04-scroll-dancer/src/components/dom/MovingDom.jsx
@@ -7,6 +7,7 @@ import { useFrame } from "@react-three/fiber";
 
 export const MovingDom = () => {
   const isEntered = useRecoilValue(IsEnteredAtom);
+  const fixed = document.getElementById('fixed');
   const scroll = useScroll();
   const article01Ref = useRef(null);
   const article02Ref = useRef(null);
@@ -18,7 +19,7 @@ export const MovingDom = () => {
   const article08Ref = useRef(null);
 
   useFrame(() => {
-    if(!isEntered || !article01Ref.current || !article02Ref.current ||  !article03Ref.current ||  !article04Ref.current || !article08Ref.current) return;
+    if(!isEntered || !fixed || !article01Ref.current || !article02Ref.current ||  !article03Ref.current ||  !article04Ref.current || !article08Ref.current) return;
     // scroll.range : 페이지가 8일 때, 0의 위치에서는 0을 리턴, 1/8의 위치에서는 1을 리턴. 그런데 여기서 1을 빼주었기 때문에 첫 위치에서 1을 리턴, 이후 1/8 지점으로 갈때 0을 리턴하는 형태
     // 결국 첫번째 페이지가 끝날 무렵 opacity가 점점 0이 되어가는 효과를 부여.
     // scroll.curve : 처음과 끝이 모두 0을 리턴, 중간에서 1을 리턴.
@@ -26,6 +27,12 @@ export const MovingDom = () => {
     article02Ref.current.style.opacity = `${1 - scroll.range(1 / 8, 1 / 8)}`;
     article03Ref.current.style.opacity = `${scroll.curve(2 / 8, 1 / 8)}`;
     article04Ref.current.style.opacity = `${scroll.curve(3 / 8, 1 / 8)}`;
+    if(scroll.visible(4/8, 3/8)) {
+      fixed.style.display = 'flex';
+      fixed.style.opacity = `${scroll.curve(4/8, 3/8)}`;
+    } else {
+      fixed.style.display = 'none';
+    }
     article08Ref.current.style.opacity = `${scroll.range(7 / 8, 1 / 8)}`;
   })
 
