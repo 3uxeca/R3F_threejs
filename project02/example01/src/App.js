ecffab51ca16f95f6185a952ad5cb2d9934ca789
3uxeca <sajihye4@gmail.com>
2025-03-13 09:55:19 +0900

    - 지구 3D오브젝트 구현

--- project02/example01/src/App.js
+++ project02/example01/src/App.js
@@ -1,36 +1,8 @@
-import { Canvas, useLoader } from '@react-three/fiber';
-import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
-
-function Model(props) {
-  const glb = useLoader(GLTFLoader, '/models/earth.glb');
-  console.log('glb :: ', glb);
-  return (
-    <mesh {...props}>
-      <primitive object={glb.scene} />
-    </mesh>
-  )
-}
-
-function Lights () {
-  return (
-    <directionalLight 
-      position={[1, 3, -1]}
-      intensity={3}
-    />
-  )
-}
+import { Home } from './pages';
 
 function App() {
   return (
-    <Canvas
-      camera={{
-        position: [0,1,5]
-      }}
-    >
-      <color attach='background' args={['yellow']}></color>
-      <Lights />
-      <Model position={[0, -2, 0]} />
-    </Canvas>
+    <Home />
   );
 }
 
