b7c0b8c540c4d3b4bee513557daff5e7572704d0
3uxeca <sajihye4@gmail.com>
2025-03-13 10:35:41 +0900

    - 날씨 Open API 소개 및 API KEY 관리 (API, 환경변수)

--- project02/example02/src/components/Scene.js
+++ project02/example02/src/components/Scene.js
@@ -1,15 +1,22 @@
+import { useEffect } from 'react';
 import Earth from "./Earth";
 import Weather from "./Weather";
+import { getCurrentWeather } from '../utils/weatherApi';
 
 const Scene = () => {
-    return(
-        <>
-            <Earth position={[0,-2,0]}/>
-            <Weather position={[0.5,0,0]} weather={'rain'}/>
-            <Weather position={[0,0,0]} weather={'clear'}/>
-            <Weather position={[-0.5,0,0]} weather={'snow'} />
-        </>
-    )
+  const API_KEY = process.env.REACT_APP_API_KEY;
+  console.log('API KEY ::', API_KEY);
+  useEffect(() => {
+      getCurrentWeather(44.34, 10.99, API_KEY);
+  }, []);
+  return(
+    <>
+      <Earth position={[0,-2,0]}/>
+      <Weather position={[0.5,0,0]} weather={'rain'}/>
+      <Weather position={[0,0,0]} weather={'clear'}/>
+      <Weather position={[-0.5,0,0]} weather={'snow'} />
+    </>
+  )
 }
 
 export default Scene;
