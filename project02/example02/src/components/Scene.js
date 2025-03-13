bca029590e66c8a77882abd4c9ed430c2ec33cb5
3uxeca <sajihye4@gmail.com>
2025-03-13 11:18:56 +0900

    - 그룹 오브젝트 해당 오브젝트만 기억하기 (useMemo)

--- project02/example02/src/components/Scene.js
+++ project02/example02/src/components/Scene.js
@@ -36,16 +36,15 @@ const Scene = () => {
   return(
     <>
       <Earth position={[0,-2,0]}/>
-      {
-        content&&
-        <>
-        <Weather position={[0.5,0,0]} weather={content[0].weatherData.weather[0].main.toLowerCase()} />
-        <Weather position={[0,0,0]} weather={content[1].weatherData.weather[0].main.toLowerCase()}/>
-        <Weather position={[-0.5,0,0]} weather={content[2].weatherData.weather[0].main.toLowerCase()} />
-        <Weather position={[-1,0,0]} weather={content[3].weatherData.weather[0].main.toLowerCase()} />
-        <Weather position={[1,0,0]} weather={content[4].weatherData.weather[0].main.toLowerCase()} />
-        </>
-      }
+      {content?.map((el, i) => {
+        return (
+          <Weather 
+            key={i+'Model Key'}
+            position={[-1 + (i * 0.5), 0, 0]}
+            weather={content[i].weatherData.weather[0].main.toLowerCase()}
+          />
+        )
+      })}
     </>
   )
 }
--- project02/example02/src/components/Weather.js
+++ project02/example02/src/components/Weather.js
@@ -1,17 +1,19 @@
 import { useLoader } from "@react-three/fiber";
+import { useMemo } from 'react';
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
 
 const Weather = (props) => {
     const { position, weather } = props;
     const glb = useLoader(GLTFLoader, '/models/weather.glb');
     console.log(glb.nodes);
-
-    let weatherModel;
-    if(glb.nodes[weather]) {
-        weatherModel = glb.nodes[weather].clone();
-    } else {
-        weatherModel = glb.nodes.cloud.clone();
-    }
+    
+    // glb.nodes에 있는 여러 모델 중 날씨에 해당하는 모델만 기억하게 하기
+    // 날씨 데이터가 바뀔 때만 재계산하고 그렇지 않으면 기억 
+    const weatherModel = useMemo(() => {
+        const cloneModel = glb.nodes[weather] || glb.nodes.cloud;
+        return cloneModel.clone();
+    }, [weather]);
+    
     return(
         <mesh position={position}>
             <primitive object={weatherModel} />
